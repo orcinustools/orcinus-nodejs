248505f72ee89060703f48aefd6de7093fc7052c
anak10thn <anak10thn@gmail.com>
2016-12-18 17:14:26 +0700

    ADD: init

--- cli.js
+++ cli.js
@@ -1,9 +1,18 @@
 var program = require('commander');
 var yml = require('yamljs');
+var pkg = require('./package.json')
 
 program
-.arguments('<url>')
-.option('-f, --file <file>', 'file list')
+.arguments('<manifest>')
+.version(pkg.version)
+.option('-c, --create', 'file list')
 .action(function(cli) {
-
+  if(file){
+    var data = parse(file);
+    console.log(data);
+  }
 }).parse(process.argv);
+
+var parse = function(file){
+  return yml.load(file);
+}
