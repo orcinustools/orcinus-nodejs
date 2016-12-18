bf8562fd4e7a6e90619c0db2d39e5cba16ecf994
anak10thn <anak10thn@gmail.com>
2016-12-18 17:20:43 +0700

    ADD: create option

--- cli.js
+++ cli.js
@@ -2,17 +2,22 @@ var program = require('commander');
 var yml = require('yamljs');
 var pkg = require('./package.json')
 
+var parser = function(file){
+  return yml.load(file);
+}
+
+var obj = function(val){
+  return Object.keys(val);
+}
+
 program
 .arguments('<manifest>')
 .version(pkg.version)
 .option('-c, --create', 'file list')
-.action(function(cli) {
+.action(function(file) {
   if(file){
-    var data = parse(file);
-    console.log(data);
+    var data = parser(file);
+    console.log(obj(data));
+    console.log(program.create)
   }
 }).parse(process.argv);
-
-var parse = function(file){
-  return yml.load(file);
-}
