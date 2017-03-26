008dbdbcf225d1a7bedc286097da8e4cea1d0af0
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-03-27 06:31:27 +0700

    Validate via docker ps

--- test/test.js
+++ test/test.js
@@ -1,13 +1,33 @@
 var should = require('should');
 var orcinusCreate = require('../lib/create');
 var chp = require('child_process');
+var fs = require('fs');
+
+
 describe('Orcinus', function() {
   describe('Create', function() {
     it('should be able to create a cluster instance from a manifest file', function(done) {
-			let cmd = 'cd ' + process.cwd() + ' && node cli.js create ../orcinus.json';
-			let data = require(process.cwd() + '/../orcinus.json');
-			orcinusCreate.init(data);
-			done();
+			this.timeout(20000);
+			let data = {
+      	"stack" : "test-stack",
+      	"services": {
+        	"web": {
+          	"image": "nginx",
+          	"cpu": "1",
+          	"memory": "128mb"
+      		}
+      	}
+			}
+			fs.writeFileSync('./test/test.json', JSON.stringify(data));
+			orcinusCreate.init(require(process.cwd() + '/test/test.json'));
+			setTimeout(() => { // wait for docker
+				let cmd = 'docker ps -f name=web1 -q';
+			  chp.exec(cmd, (err, stdout, stderr) => {
+					stdout.length.should.greaterThan(10); // Container ID length was 12
+					chp.exec('cd ' + process.cwd() + ' && node cli.js rm -f test/test.json');
+			  	done();
+			  });
+			}, 15000);
     });
   });
 });
