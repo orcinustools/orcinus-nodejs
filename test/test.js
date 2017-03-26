c51f1ee383aee0b910f79562fc037b9952dd7f19
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-03-27 06:50:46 +0700

    Test both via API and command

--- test/test.js
+++ test/test.js
@@ -2,15 +2,37 @@ var should = require('should');
 var chp = require('child_process');
 var fs = require('fs');
 var orcinusCreate = require('../lib/create');
+var orcinusRemove = require('../lib/rm');
 
 // TODO fix me
 // Check for docker binary and `nginx` image before running unit testing
-// var orcinusRemove = require('../lib/rm');
 // var orcinusPs = require('../lib/ps');
 
 
 describe('Orcinus', function() {
   describe('Create', function() {
+    it('should be able to create a cluster instance via API', function(done) {
+			this.timeout(20000);
+			let data = {
+      	"stack" : "test-stack",
+      	"services": {
+        	"web": {
+          	"image": "nginx",
+          	"cpu": "1",
+          	"memory": "128mb"
+      		}
+      	}
+			}
+			orcinusCreate.init(data);
+			setTimeout(() => { // wait for docker
+				let cmd = 'docker ps -f name=web1 -q';
+			  chp.exec(cmd, (err, stdout, stderr) => {
+					stdout.length.should.greaterThan(10); // Container ID length was 12
+					orcinusRemove.init(data);
+			  	done();
+			  });
+			}, 15000);
+    });
     it('should be able to create a cluster instance from a manifest file', function(done) {
 			this.timeout(20000);
 			let data = {
@@ -24,15 +46,16 @@ describe('Orcinus', function() {
       	}
 			}
 			fs.writeFileSync('./test/test.json', JSON.stringify(data));
-			orcinusCreate.init(require(process.cwd() + '/test/test.json'));
-			setTimeout(() => { // wait for docker
-				let cmd = 'docker ps -f name=web1 -q';
-			  chp.exec(cmd, (err, stdout, stderr) => {
-					stdout.length.should.greaterThan(10); // Container ID length was 12
-					chp.exec('cd ' + process.cwd() + ' && node cli.js rm -f test/test.json');
-			  	done();
-			  });
-			}, 15000);
+			chp.exec('cd ' + process.cwd() + ' && node cli.js test/test.json', () => {
+			  setTimeout(() => { // wait for docker
+			  	let cmd = 'docker ps -f name=web1 -q';
+			    chp.exec(cmd, (err, stdout, stderr) => {
+			  		stdout.length.should.greaterThan(10); // Container ID length was 12
+			  		chp.exec('cd ' + process.cwd() + ' && node cli.js rm -f test/test.json');
+			    	done();
+			    });
+			  }, 15000);
+			});
     });
   });
 });
