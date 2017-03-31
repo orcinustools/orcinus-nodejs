a94fb1554666299dbdb70e481cf318788e2e61eb
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-04-01 04:21:30 +0700

    Pre and post script for test

--- test/pre-post.js
+++ test/pre-post.js
@@ -1,20 +1,27 @@
 'use strict';
 
+const mode = process.argv[2];
+
 var chp = require('child_process');
 var async = require('async');
 
 var cmds = [
-	// TODO check for nginx image
   'cd ' + process.cwd() + ' && node cli.js cluster leave',
   'cd ' + process.cwd() + ' && node cli.js cluster leave-manager',
   'cd ' + process.cwd() + ' && node cli.js cluster init 127.0.0.1',
   'docker network rm orcinus-unit-testing-stack',
 ];
 
+if (mode === 'post') {
+	cmds.splice(2,1);
+}
+
 async.eachSeries(cmds, (cmd, cb) => {
 	chp.exec(cmd, (err, stdout, stderr) => {
 		cb();
 	});
 }, () => {
-  console.log('Ready to test');
+	if (mode === 'pre') {
+  	console.log('Ready to test');
+	}
 });
