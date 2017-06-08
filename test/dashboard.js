6e13f11554fcb6f4e5d1bf98544f78b54bc65f71
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-06-08 19:35:58 +0700

    Basic unit test for HTTP apis

--- test/dashboard.js
+++ test/dashboard.js
@@ -1,10 +1,13 @@
 'use strict';
 
 global.test = true;
+    
+process.env.ORCINUS_HOST = '127.0.0.1';
+process.env.ORCINUS_PORT = '4000';
+process.env.ORCINUS_HTTP_CORS = '*'
 
 var chp = require('child_process');
-var config = require('./config');
-var server = require('../server')(config);
+var server = require('../server')();
 var request = require('supertest');
 var should = require('should');
 
@@ -32,8 +35,25 @@ describe('Orcinus Dashboard', function() {
           var keys = Object.keys(res.body);
           for (var i in keys) {
             if (keys[i] == 'SystemTime') continue;
-            console.log(res.body[keys[i]]);
-            should(JSON.stringify(res.body[keys[i]])).equal(JSON.stringify(result[keys[i]]));
+            if (keys[i] == 'Swarm') {
+              var swarmKeys = Object.keys(res.body[keys[i]]);
+              for (var j in swarmKeys) {
+                // Catch the undetermined addr value!
+                if (swarmKeys[j] == 'RemoteManagers') {
+                  for (var k in res.body[keys[i]][swarmKeys[j]]['RemoteManagers']) {
+                    let ret = (res.body[keys[i]][swarmKeys[j]]['RemoteManagers']['NodeID'] == result[keys[i]][swarmKeys[j]]['RemoteManagers']['NodeID'])
+                    should(ret).equal(true);
+                    ret = (res.body[keys[i]][swarmKeys[j]]['RemoteManagers']['NodeID'] == '127.0.0.1:2337' || '0.0.0.0:2337');
+                    should(ret).equal(true);
+                  }
+                } else {
+                  should(JSON.stringify(res.body[keys[i]][swarmKeys[j]]))
+                  .equal(JSON.stringify(result[keys[i]][swarmKeys[j]]));
+                }
+              }
+            } else {
+              should(JSON.stringify(res.body[keys[i]])).equal(JSON.stringify(result[keys[i]]));
+            }
           }
           done();
         });
