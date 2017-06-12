c5eeb115b29a66d247a2bc582866a071b7a1f8bc
anak10thn <anak10thn@gmail.com>
2017-06-12 21:11:59 +0700

    ADD: x-access-token to cors header

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ module.exports = function(){
   if(CORS){
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", CORS);
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
       next();
     });
   }
