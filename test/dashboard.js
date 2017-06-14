9c87c5e163f8c3439b0d1f923510aa325f3eb4e3
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-06-15 04:02:02 +0700

    Add auth test. Keep up with current code base.

--- test/dashboard.js
+++ test/dashboard.js
@@ -5,22 +5,72 @@ global.test = true;
 process.env.ORCINUS_HOST = '127.0.0.1';
 process.env.ORCINUS_PORT = '4000';
 process.env.ORCINUS_HTTP_CORS = '*'
+process.env.ORCINUS_DB = 'localhost';
 
+var pkg = require(__dirname + '/../package.json');
+var mongoose = require('mongoose');
+mongoose.connection.dropDatabase('test');
 var chp = require('child_process');
 var server = require('../server')();
 var request = require('supertest');
 var should = require('should');
+var faker = require('faker');
+var password = faker.internet.password();
+var token;
 
 const timeout = 5000;
 
+// TODO : drop test db before testing
+
 describe('Orcinus Dashboard', function() {
   describe('API', function() {
+    it('Signup', function(done) {
+      this.timeout(timeout);
+      request(server)
+      .post('/apis/auth/signup')
+      .send({ 
+        username : 'admin',
+        password : password,
+        email : faker.internet.email(),
+        firstname : faker.name.firstName(),
+        lastname : faker.name.lastName(),
+        admin : true,
+      })
+      .end((err, res) => {
+        var result = JSON.parse(res.text);
+        done();
+      });
+    });
+    it('Get token', function(done) {
+      this.timeout(timeout);
+      request(server)
+      .post('/apis/auth/signin')
+      .send({ 
+        username : 'admin',
+        password : password,
+      })
+      .end((err, res) => {
+        var result = JSON.parse(res.text);
+        token = result.token;
+        done();
+      });
+    });
     it('Ping', function(done) {
       this.timeout(timeout);
       request(server)
       .get('/apis/ping')
+      .set('X-Access-Token', token)
       .end((err, res) => {
         should(res.text).equal('OK');
+        done();
+      });
+    });
+    it('Unauthorized', function(done) {
+      this.timeout(timeout);
+      request(server)
+      .get('/apis/ping')
+      .end((err, res) => {
+        should(res.text).equal('{"success":false,"message":"No token provided."}');
         done();
       });
     });
@@ -28,35 +78,15 @@ describe('Orcinus Dashboard', function() {
       this.timeout(timeout);
       request(server)
       .get('/apis/info')
+      .set('X-Access-Token', token)
       .end((err, res) => {
-        let cmd = 'docker info --format \'{{json .}}\'';
-        chp.exec(cmd, (err, stdout, stderr) => {
-          var result = JSON.parse(stdout);
-          var keys = Object.keys(res.body);
-          for (var i in keys) {
-            if (keys[i] == 'SystemTime') continue;
-            if (keys[i] == 'Swarm') {
-              var swarmKeys = Object.keys(res.body[keys[i]]);
-              for (var j in swarmKeys) {
-                // Catch the undetermined addr value!
-                if (swarmKeys[j] == 'RemoteManagers') {
-                  for (var k in res.body[keys[i]][swarmKeys[j]]['RemoteManagers']) {
-                    let ret = (res.body[keys[i]][swarmKeys[j]]['RemoteManagers']['NodeID'] == result[keys[i]][swarmKeys[j]]['RemoteManagers']['NodeID'])
-                    should(ret).equal(true);
-                    ret = (res.body[keys[i]][swarmKeys[j]]['RemoteManagers']['NodeID'] == '127.0.0.1:2337' || '0.0.0.0:2337');
-                    should(ret).equal(true);
-                  }
-                } else {
-                  should(JSON.stringify(res.body[keys[i]][swarmKeys[j]]))
-                  .equal(JSON.stringify(result[keys[i]][swarmKeys[j]]));
-                }
-              }
-            } else {
-              should(JSON.stringify(res.body[keys[i]])).equal(JSON.stringify(result[keys[i]]));
-            }
-          }
-          done();
-        });
+        should(res.body.name).equal(pkg.name);
+        should(res.body.version).equal(pkg.version);
+        should(res.body.description).equal(pkg.description);
+        should(res.body.bugs.url).equal(pkg.bugs.url);
+        should(res.body.deployment).equal('dev');
+        should(res.body.cors).equal(process.env.ORCINUS_HTTP_CORS);
+        done();
       });
     });
   });
