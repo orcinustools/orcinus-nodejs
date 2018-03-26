65cf14a4e345736bb634b9c2634342f0b77609bb
anak10thn <anak10thn@gmail.com>
2018-03-26 12:43:31 +0700

    ADD: mongoclient

--- db/index.js
+++ db/index.js
@@ -2,7 +2,12 @@ var mongoose	= require('mongoose');
 var colors = require('colors');
 
 var db = function(host){
-	mongoose.connect(host, function (err, res) {
+	mongoose.connect(host,
+		{
+		  useMongoClient: true,
+		  /* other options */
+		},
+		function (err, res) {
 	  if (err) {
 	    console.log (colors.red('==> ERROR connecting to: ' + host + '. ' + err));
 	  } else {
